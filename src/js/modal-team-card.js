e3badb1865c44b9a95dbf2e895777e4f478f94a4
Tin Pasco <tinontengco@gmail.com>
2024-04-09 22:02:13 +0800

    update team card

--- src/js/modal-team-card.js
+++ src/js/modal-team-card.js
@@ -1,12 +1,20 @@
 // Get the modal
+<<<<<<< Updated upstream
 var modal = document.getElementById("teamModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("openTeamModalBtn");
+=======
+var modal = document.getElementById('teamModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModalBtn");
+>>>>>>> Stashed changes
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+<<<<<<< Updated upstream
 // Function to open the modal
 function openModal() {
   modal.classList.remove("is-hidden");
@@ -20,16 +28,31 @@ function closeModal() {
 // When the user clicks the button, open the modal
 btn.onclick = function() {
   openModal();
+=======
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+>>>>>>> Stashed changes
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+<<<<<<< Updated upstream
   closeModal();
+=======
+  modal.style.display = "none";
+>>>>>>> Stashed changes
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
+<<<<<<< Updated upstream
     closeModal();
   }
 }
+=======
+    modal.style.display = "none";
+  }
+}
+>>>>>>> Stashed changes
