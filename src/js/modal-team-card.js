a576ba1dc15f93b8532398197820d31689212963
Tin Pasco <tinontengco@gmail.com>
2024-04-10 19:55:19 +0800

    update modal-team-card.js

--- src/js/modal-team-card.js
+++ src/js/modal-team-card.js
@@ -1,19 +1,14 @@
 // Get the modal
 var modal = document.getElementById('teamModal');
 
-<<<<<<< Updated upstream
 // Get the button that opens the modal
-var btn = document.getElementById("openModalBtn");
-=======
-// Get the trigger that opens the modal
-var trigger = document.getElementByClass("js-team-modal team-modal");
->>>>>>> Stashed changes
+var text = document.getElementsByClassName("js-team-modal team-modal");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+text.onclick = function() {
   modal.style.display = "block";
 }
 
